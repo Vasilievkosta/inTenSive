57c3cabe741f2e6142baea91df53baf10684d0a7
Vasilievkosta <vasilievkosta@gmail.com>
2024-07-05 20:41:30 +0300

    feat: add less-03-01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,7 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(f => f === "черный" || f === "красный" || f === "желтый").join("-")
+}
+
+console.log(createColorString());
