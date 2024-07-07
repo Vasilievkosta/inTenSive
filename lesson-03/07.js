0440eb819c40fe72a19b7d0af4056bce1ecbee3b
Vasilievkosta <vasilievkosta@gmail.com>
2024-07-07 20:35:29 +0300

    feat: add less-03-07

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -1,1 +1,8 @@
-//code
+ const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+  
+const result = numbers.map(arr => arr.filter(f => f % 2 === 0))
+console.log(result)
